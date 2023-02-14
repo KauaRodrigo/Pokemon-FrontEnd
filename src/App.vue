d9df5d94d4658e369ccf69647e1cc1fbc113cfdf
KauaRodrigo <kaurodrigo.joenck2@gmail.com>
2023-02-14 20:48:21 -0300

    voltando alterações de teste

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,31 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
-import { onMounted } from "vue";
-
-onMounted(() => {
-  setTimeout(() => {
-    document.getElementsByClassName('glue')[0].style.display = "none"
-  }, 10000)
-})
 
 </script>
 
 <template>
-  <div class="glue">
-    <div class="semi-ball"></div>
-    <div class="semi-ball"></div>
-    <div class="ball">
-      <div class="small-ball"></div>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
-  </div>
+  </header>
+
+
+  <RouterView />
 </template>
 
 <style scoped>
-.semi-ball:nth-child(1) {
-  width: 90px;
-  height: 45px;
-  border-radius: 12rem 12rem 0 0;
-  border: 4px solid black;
-  background-color: red;
-}
-.semi-ball:nth-child(2) {
-  width: 90px;
-  height: 45px;
-  border-radius: 0 0 12rem 12rem;
-  border: 4px solid black;
-  background-color: white;
-}
 
-.glue {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  animation: rotacao 2s linear infinite;  
-  position: absolute;
-  top: 400px;
-  left: -200px;
-}
-
-@keyframes rotacao {
-  0% {
-    rotate: 0deg;    
-  }
-  25% {
-    rotate: 90deg;
-    top: 350px;
-    left: 100px;
-  }
-  50% {
-    rotate: 180deg;
-    top: 400px;
-    left: 500px;
-  }
-  75% {
-    rotate: 240deg;
-    top: 350px;
-    left: 950px;
-  }
-  100% {
-    rotate: 360deg;
-    top: 400px;
-    left: 1400px;
-  }
-}
-
-.small-ball {
-  width: 12.25px;
-  height: 12.25px;
-  border-radius: 12rem 12rem;
-  background-color: gray;
-}
-
-.ball {
-  position: absolute;
-  top: 38%;
-  width: 24.5px;
-  height: 24.5px;
-  border-radius: 12rem 12rem;
-  rotate: 270deg;
-  display: grid;
-  place-items: center;
-  border: 5px solid black;
-  background-color: white;
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
 .logo {
